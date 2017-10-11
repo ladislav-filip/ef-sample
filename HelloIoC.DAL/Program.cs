7568cfae3c9a7c74871fa2df900bccfea31538ee
Martin Dybal <martin@dybal.it>
2017-10-11 15:48:13 +0200

    Sample

--- HelloIoC.DAL/Program.cs
+++ HelloIoC.DAL/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HelloIoC.DAL.DTO;
+using HelloIoC.DAL.Facade;
+using HelloIoC.DAL.Query;
+using HelloIoC.DAL.Repository;
 
 namespace HelloIoC.DAL
 {
@@ -10,6 +14,22 @@ namespace HelloIoC.DAL
     {
         static void Main(string[] args)
         {
+            var mapper = new Mapper();
+
+            var contactListFacade = new ContactListFacade(new ContactRepository(mapper), () => new AllContactQuery(new ContactFirstLevelQuery(), mapper));
+
+            var contact1 = contactListFacade.GetDetail(1);
+            PrintContactDetail(contact1);
+
+            foreach (var contact in contactListFacade.GetAllContacts())
+            {
+                PrintContactDetail(contact);
+            }
+        }
+
+        private static void PrintContactDetail(ContactDTO contact)
+        {
+            Console.WriteLine($"{contact.Name}: {contact.Email}");
         }
     }
 }
