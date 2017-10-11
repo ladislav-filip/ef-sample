98a486eb93a39009c3dbbf0adf800456027de76e
Martin Dybal <martin@dybal.it>
2017-10-11 14:59:38 +0200

    Add interfaces for query

---
+++ HelloIoC.DAL/Query/IQuery.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloIoC.DAL.Query
+{
+    public interface IFirstLevelQuery<out TEntity>
+    {
+        IQueryable<TEntity> GetSource();
+    }
+    
+    public interface IQuery<TDTO>
+    {
+        int PageSize { get; }
+
+        int PageIndex { get; }
+        
+        IList<TDTO> Execute();
+    }
+
+    interface IFilterQuery<TFilter, TDTO, TEntity> : IQuery<TDTO>
+    {
+        TFilter Filter { get; }
+    }
+}
