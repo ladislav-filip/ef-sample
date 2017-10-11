fe2f910eb858ac0bf940169dcc1a81442117daeb
Martin Dybal <martin@dybal.it>
2017-10-11 14:50:11 +0200

    add repository andmapper

---
+++ HelloIoC.DAL/Repository/ContactRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using HelloIoC.DAL.DTO;
+
+namespace HelloIoC.DAL.Repository
+{
+    public class ContactRepository
+    {
+        private readonly Mapper mapper;
+
+        public ContactRepository(Mapper mapper)
+        {
+            this.mapper = mapper;
+        }
+
+        public ContactDTO GetById(int id)
+        {
+            using (var db = new ContactDbContext())
+            {
+                //() => Email
+                //var memberExpression = mapper.MapContactToContactDTOExpression.Body as MemberExpression;
+                //memberExpression.Member.Name
+                
+                //var contactDTO = db.Contacts.Select(mapper.MapContactToContactDTOExpression).FirstOrDefault(c => c.Id == id);
+                //return contactDTO;
+
+                var contact = db.Contacts.Find(id);
+                return mapper.MapContactToContactDTO(contact);
+            }
+        }
+    }
+
+    public class Mapper
+    {
+        public ContactDTO MapContactToContactDTO(Contact contact)
+        {
+            return MapContactToContactDTOExpression.Compile()(contact);
+        }
+
+        public Expression<Func<Contact, ContactDTO>> MapContactToContactDTOExpression { get; } = c => new ContactDTO
+        {
+            Id = c.Id,
+            Name = c.Firstname + " " + c.Lastname,
+            Email = c.Email,
+            PhoneNumber = c.PhoneNumber
+        };
+    }
+}
---
+++ HelloIoC.DAL/Services/Mapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq.Expressions;
+using HelloIoC.DAL.DTO;
+
+namespace HelloIoC.DAL.Repository
+{
+    public class Mapper
+    {
+        public ContactDTO MapContactToContactDTO(Contact contact)
+        {
+            var method = MapContactToContactDTOExpression.Compile();
+            return method(contact);
+        }
+
+        public Expression<Func<Contact, ContactDTO>> MapContactToContactDTOExpression { get; } = c => new ContactDTO
+        {
+            Id = c.Id,
+            Name = c.Firstname + " " + c.Lastname,
+            Email = c.Email,
+            PhoneNumber = c.PhoneNumber
+        };
+    }
+}
